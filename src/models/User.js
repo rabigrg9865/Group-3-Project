23e53150c09716c8fa31f813ff2d3134b4214903
Bibek Itani <43322077+bibekitani34@users.noreply.github.com>
2025-05-25 12:11:45 -0400

    Delete src directory

--- src/models/User.js
+++
@@ -1,9 +0,0 @@
-// This file defines the User model for the application.
-class User {
-    // Constructor to initialize a User object
-    constructor(username) {
-      this.username = username;
-    }
-  }
-  module.exports = User;
-  
